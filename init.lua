982237d140362d14f836c237d22acf2ab67fa7a7
Venceslas <duet.venceslas@orange.fr>
2016-04-12 14:58:13 +0200

    Added files via upload

---
+++ init.lua
@@ -0,0 +1,283 @@
+-- Clay Mod
+-- By Citorva
+--
+-- License: Do what you want but give credit where credit is due
+--
+-- NOTES: I'm not responsible for anything. Use at your own risk.
+-- Any and everything in here used under fair use.
+
+-- Blocks
+--     Clay Block
+minetest.register_node("clay:clay_block", {
+	description = "Clay Block",
+	tiles = {"clay_normal.png"},
+	inventory_image = minetest.inventorycube("clay_normal.png"),
+	is_ground_content = true,
+	groups = {cracky=3},
+	sounds = default.node_sound_dirt_defaults(),
+})
+--     End clay block
+--     Baked clay Block
+minetest.register_node("clay:baked_clay_block", {
+	description = "Baked Clay Block",
+	tiles = {"clay_baked.png"},
+	inventory_image = minetest.inventorycube("clay_baked.png"),
+	is_ground_content = true,
+	groups = {cracky=3},
+	sounds = default.node_sound_stone_defaults(),
+})
+--     End Baked clay block
+-- Baked black clay Block
+minetest.register_node("clay:baked_black_clay_block", {
+description = "Baked black Clay Block",
+tiles = {"clay_black.png"},
+inventory_image = minetest.inventorycube("clay_black.png"),
+is_ground_content = true,
+groups = {cracky=3},
+sounds = default.node_sound_stone_defaults(),
+})
+-- End Baked black clay block
+-- Baked blue clay Block
+minetest.register_node("clay:baked_blue_clay_block", {
+description = "Baked blue Clay Block",
+tiles = {"clay_blue.png"},
+inventory_image = minetest.inventorycube("clay_blue.png"),
+is_ground_content = true,
+groups = {cracky=3},
+sounds = default.node_sound_stone_defaults(),
+})
+-- End Baked blue clay block
+-- Baked brown clay Block
+minetest.register_node("clay:baked_brown_clay_block", {
+description = "Baked brown Clay Block",
+tiles = {"clay_brown.png"},
+inventory_image = minetest.inventorycube("clay_brown.png"),
+is_ground_content = true,
+groups = {cracky=3},
+sounds = default.node_sound_stone_defaults(),
+})
+-- End Baked brown clay block
+-- Baked cyan clay Block
+minetest.register_node("clay:baked_cyan_clay_block", {
+description = "Baked cyan Clay Block",
+tiles = {"clay_cyan.png"},
+inventory_image = minetest.inventorycube("clay_cyan.png"),
+is_ground_content = true,
+groups = {cracky=3},
+sounds = default.node_sound_stone_defaults(),
+})
+-- End Baked cyan clay block
+-- Baked dark_green clay Block
+minetest.register_node("clay:baked_dark_green_clay_block", {
+description = "Baked dark_green Clay Block",
+tiles = {"clay_dark_green.png"},
+inventory_image = minetest.inventorycube("clay_dark_green.png"),
+is_ground_content = true,
+groups = {cracky=3},
+sounds = default.node_sound_stone_defaults(),
+})
+-- End Baked dark_green clay block
+-- Baked dark_grey clay Block
+minetest.register_node("clay:baked_dark_grey_clay_block", {
+description = "Baked dark_grey Clay Block",
+tiles = {"clay_dark_grey.png"},
+inventory_image = minetest.inventorycube("clay_dark_grey.png"),
+is_ground_content = true,
+groups = {cracky=3},
+sounds = default.node_sound_stone_defaults(),
+})
+-- End Baked dark_grey clay block
+-- Baked green clay Block
+minetest.register_node("clay:baked_green_clay_block", {
+description = "Baked green Clay Block",
+tiles = {"clay_green.png"},
+inventory_image = minetest.inventorycube("clay_green.png"),
+is_ground_content = true,
+groups = {cracky=3},
+sounds = default.node_sound_stone_defaults(),
+})
+-- End Baked green clay block
+-- Baked grey clay Block
+minetest.register_node("clay:baked_grey_clay_block", {
+description = "Baked grey Clay Block",
+tiles = {"clay_grey.png"},
+inventory_image = minetest.inventorycube("clay_grey.png"),
+is_ground_content = true,
+groups = {cracky=3},
+sounds = default.node_sound_stone_defaults(),
+})
+-- End Baked grey clay block
+-- Baked magenta clay Block
+minetest.register_node("clay:baked_magenta_clay_block", {
+description = "Baked magenta Clay Block",
+tiles = {"clay_magenta.png"},
+inventory_image = minetest.inventorycube("clay_magenta.png"),
+is_ground_content = true,
+groups = {cracky=3},
+sounds = default.node_sound_stone_defaults(),
+})
+-- End Baked magenta clay block
+-- Baked orange clay Block
+minetest.register_node("clay:baked_orange_clay_block", {
+description = "Baked orange Clay Block",
+tiles = {"clay_orange.png"},
+inventory_image = minetest.inventorycube("clay_orange.png"),
+is_ground_content = true,
+groups = {cracky=3},
+sounds = default.node_sound_stone_defaults(),
+})
+-- End Baked orange clay block
+-- Baked pink clay Block
+minetest.register_node("clay:baked_pink_clay_block", {
+description = "Baked pink Clay Block",
+tiles = {"clay_pink.png"},
+inventory_image = minetest.inventorycube("clay_pink.png"),
+is_ground_content = true,
+groups = {cracky=3},
+sounds = default.node_sound_stone_defaults(),
+})
+-- End Baked pink clay block
+-- Baked purple clay Block
+minetest.register_node("clay:baked_purple_clay_block", {
+description = "Baked purple Clay Block",
+tiles = {"clay_purple.png"},
+inventory_image = minetest.inventorycube("clay_purple.png"),
+is_ground_content = true,
+groups = {cracky=3},
+sounds = default.node_sound_stone_defaults(),
+})
+-- End Baked purple clay block
+-- Baked red clay Block
+minetest.register_node("clay:baked_red_clay_block", {
+description = "Baked red Clay Block",
+tiles = {"clay_red.png"},
+inventory_image = minetest.inventorycube("clay_red.png"),
+is_ground_content = true,
+groups = {cracky=3},
+sounds = default.node_sound_stone_defaults(),
+})
+-- End Baked red clay block
+-- Baked violet clay Block
+minetest.register_node("clay:baked_violet_clay_block", {
+description = "Baked violet Clay Block",
+tiles = {"clay_violet.png"},
+inventory_image = minetest.inventorycube("clay_violet.png"),
+is_ground_content = true,
+groups = {cracky=3},
+sounds = default.node_sound_stone_defaults(),
+})
+-- End Baked violet clay block
+-- Baked white clay Block
+minetest.register_node("clay:baked_white_clay_block", {
+description = "Baked white Clay Block",
+tiles = {"clay_white.png"},
+inventory_image = minetest.inventorycube("clay_white.png"),
+is_ground_content = true,
+groups = {cracky=3},
+sounds = default.node_sound_stone_defaults(),
+})
+-- End Baked white clay block
+-- Baked yellow clay Block
+minetest.register_node("clay:baked_yellow_clay_block", {
+description = "Baked yellow Clay Block",
+tiles = {"clay_yellow.png"},
+inventory_image = minetest.inventorycube("clay_yellow.png"),
+is_ground_content = true,
+groups = {cracky=3},
+sounds = default.node_sound_stone_defaults(),
+})
+-- End Baked yellow clay block
+-- Craft
+minetest.register_craft({
+	type   = "cooking",
+	output = 'clay:baked_clay_block',
+	recipe = 'clay:clay_block',
+})
+minetest.register_craft({
+	output = 'node "clay:clay_block"',
+	recipe = {
+		{'node "default:clay_lump"', 'node "default:clay_lump"', 'node "default:clay_lump"'},
+		{'node "default:clay_lump"', 'node "default:clay_lump"', 'node "default:clay_lump"'},
+		{'node "default:clay_lump"', 'node "default:clay_lump"', 'node "default:clay_lump"'},
+	}
+})
+minetest.register_craft({
+type = "shapeless",
+output = 'node "clay:baked_black_clay_block"',
+recipe = {'node "clay:baked_clay_block"', 'dye:black'}
+})
+minetest.register_craft({
+type = "shapeless",
+output = 'node "clay:baked_blue_clay_block"',
+recipe = {'node "clay:baked_clay_block"', 'dye:blue'}
+})
+minetest.register_craft({
+type = "shapeless",
+output = 'node "clay:baked_brown_clay_block"',
+recipe = {'node "clay:baked_clay_block"', 'dye:brown'}
+})
+minetest.register_craft({
+type = "shapeless",
+output = 'node "clay:baked_cyan_clay_block"',
+recipe = {'node "clay:baked_clay_block"', 'dye:cyan'}
+})
+minetest.register_craft({
+type = "shapeless",
+output = 'node "clay:baked_dark_green_clay_block"',
+recipe = {'node "clay:baked_clay_block"', 'dye:dark_green'}
+})
+minetest.register_craft({
+type = "shapeless",
+output = 'node "clay:baked_dark_grey_clay_block"',
+recipe = {'node "clay:baked_clay_block"', 'dye:dark_grey'}
+})
+minetest.register_craft({
+type = "shapeless",
+output = 'node "clay:baked_green_clay_block"',
+recipe = {'node "clay:baked_clay_block"', 'dye:green'}
+})
+minetest.register_craft({
+type = "shapeless",
+output = 'node "clay:baked_grey_clay_block"',
+recipe = {'node "clay:baked_clay_block"', 'dye:grey'}
+})
+minetest.register_craft({
+type = "shapeless",
+output = 'node "clay:baked_magenta_clay_block"',
+recipe = {'node "clay:baked_clay_block"', 'dye:magenta'}
+})
+minetest.register_craft({
+type = "shapeless",
+output = 'node "clay:baked_orange_clay_block"',
+recipe = {'node "clay:baked_clay_block"', 'dye:orange'}
+})
+minetest.register_craft({
+type = "shapeless",
+output = 'node "clay:baked_pink_clay_block"',
+recipe = {'node "clay:baked_clay_block"', 'dye:pink'}
+})
+minetest.register_craft({
+type = "shapeless",
+output = 'node "clay:baked_purple_clay_block"',
+recipe = {'node "clay:baked_clay_block"', 'dye:purple'}
+})
+minetest.register_craft({
+type = "shapeless",
+output = 'node "clay:baked_red_clay_block"',
+recipe = {'node "clay:baked_clay_block"', 'dye:red'}
+})
+minetest.register_craft({
+type = "shapeless",
+output = 'node "clay:baked_violet_clay_block"',
+recipe = {'node "clay:baked_clay_block"', 'dye:violet'}
+})
+minetest.register_craft({
+type = "shapeless",
+output = 'node "clay:baked_white_clay_block"',
+recipe = {'node "clay:baked_clay_block"', 'dye:white'}
+})
+minetest.register_craft({
+type = "shapeless",
+output = 'node "clay:baked_yellow_clay_block"',
+recipe = {'node "clay:baked_clay_block"', 'dye:yellow'}
+})
